07c0ce92324b06356a31178c2cf95f616d5e45f7
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-31 02:14:13 +0200

    docs: add javadoc

--- app/src/main/java/de/pascalkuehnold/tipcalculator/ui/screens/MainContent.kt
+++ app/src/main/java/de/pascalkuehnold/tipcalculator/ui/screens/MainContent.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import de.pascalkuehnold.tipcalculator.domain.BillFormViewModel
 import de.pascalkuehnold.tipcalculator.ui.screens.billform.components.BillForm
 import de.pascalkuehnold.tipcalculator.ui.theme.TipCalculatorTheme
@@ -12,8 +11,13 @@ import de.pascalkuehnold.tipcalculator.ui.theme.TipCalculatorTheme
 /**
  * Created by Pascal Kühnold on 7/14/2022.
  */
+
+/**
+ * Composable Function to display the main content
+ *
+ * @property range The range for the tip slider
+ */
 @Composable
-@Preview
 fun MainContent() {
     val range = IntRange(start = 1, endInclusive = 100)
     val billFormViewModel = BillFormViewModel()
